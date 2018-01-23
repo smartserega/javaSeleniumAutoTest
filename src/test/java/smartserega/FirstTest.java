f916d21845d2e9c7e23b56568f8d9a314c6088c7
Sergey <smartserega85@gmail.com>
2018-01-23 15:05:57 +0300

    java_selenium_auto_test-2

--- src/test/java/smartserega/FirstTest.java
+++ src/test/java/smartserega/FirstTest.java
@@ -15,21 +15,26 @@ public class FirstTest {
 
     @BeforeClass
     public static void setup() {
-/*        System.setProperty("webdriver.chrome.driver", "chromedriver");
-        driver = new ChromeDriver();*/
-
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://google.com");
+        driver.get("https://www.tutu.ru/");
     }
 
-    @Test
+
+   /* Сценарий:Расписание
+    Когда Пользователь заходит на сайт "Туту-ру" (https://www.tutu.ru/
+    И вводит станцию отправления "Петровско-Разумовская"
+    И вводит конечный пунт назначения "Тверь"
+    И нажимает кнопку "Найти"
+    Тогда Пользователь может увидеть расписание поездов "Петровско-Разумовская Тверь"*/
+
+
+
+
+ /*   @Test
     public void userLogin() {
-       /* ТУТ НЕ МОЙ АВТОТЕСТ НАЧИНАЕНТСЯ (т.е. селекторы и прочье не моё),
-       Я СВОИ ХОЧУ ПОДСТАВИТЬ ЭЛЛЕМЕНТЫ, МНЕ ГЛАВНОЕ ПОНЯТЬ КАК ГУГЛ ЗАПУСТИТЬ,
-       ДЕЛАТЬ КЛИКИ, ТЕКСТ ПИСАТЬ И Т.Д. Т,Е, ВВСЕ МЕТОДЫ НЕОБЪОДИМЫ ДЛЯ ТЕСТИРОВАНИЯ
 
 
         WebElement loginField = driver.findElement(By.id("login"));
@@ -40,15 +45,15 @@ public class FirstTest {
         loginButton.click();
         WebElement profileUser = driver.findElement(By.cssSelector(".login-button__user"));
         String mailUser = profileUser.getText();
-        Assert.assertEquals("autotestorgua@ukr.net", mailUser);*/
+        Assert.assertEquals("autotestorgua@ukr.net", mailUser);
     }
 
     @AfterClass
     public static void tearDown() {
-        /*WebElement menuUser = driver.findElement(By.cssSelector(".login-button__menu-icon"));
+        WebElement menuUser = driver.findElement(By.cssSelector(".login-button__menu-icon"));
         menuUser.click();
         WebElement logoutButton = driver.findElement(By.id("login__logout"));
         logoutButton.click();
-        driver.quit();*/
-    }
+        driver.quit();
+    }*/
 }
